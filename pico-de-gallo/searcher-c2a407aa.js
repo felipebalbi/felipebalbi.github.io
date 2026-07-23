11980080c00bdc09671b0af17326a8ada458ab2b
felipebalbi <felipebalbi@users.noreply.github.com>
2026-07-23 18:41:27 +0000

    deploy: OpenDevicePartnership/pico-de-gallo@641840dc520935c2f572f029f08c038d00e962d0

--- pico-de-gallo/searcher-c2a407aa.js
+++ pico-de-gallo/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-70145703.js',
+                path_to_root + 'searchindex-1ba74092.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
